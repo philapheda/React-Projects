6c61df6d556e3558e042b4184343ab4721ad588a
AbrhamYishak <136889996+philapheda@users.noreply.github.com>
2024-10-20 14:48:03 +0300

    hi
    hi

--- Meme-generator/src/components/Main.jsx
+++ Meme-generator/src/components/Main.jsx
@@ -9,25 +9,34 @@ function Main() {
     let randomim = datas.data.memes[Math.floor(Math.random()*(datas.data.memes.length))].url
     setimage(randomim)
   }
+  let [top, settop] = useState("")
+  let [below, setbelow] = useState("")
+  function write(){
+    settop(document.getElementById('top').value)
+  }
+  function writel(){
+    setbelow(document.getElementById('bel').value)
+  }
+  console.log("hi")
  return (
     <div className="Main">
       <div className="fields">
         <div className="field">
           <label>Top text</label>
-          <input placeholder="  Shut up" type="text"></input>
+          <input placeholder="  Shut up" type="text" id = "top" onKeyDown={write}></input>
         </div>
         <div className="field">
           <label>Bottom text</label>
-          <input placeholder="  And take my money" type="text"></input>
+          <input placeholder="  And take my money" type="text" id = "bel"  onKeyDown={writel}></input>
         </div>
         <button>
           <img src={btn} onClick={generate}/>
         </button>
       </div>
       <div className="image">
-        <p></p>
+        <p className="top">{top}</p>
             <img src={image}/>
-          <p></p>
+          <p className="bot">{below}</p>
         </div>
     </div>
   );
